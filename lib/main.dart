fda3ffea5d752f999b27e13eb81ba43acbf17d50
jefrin26 <jefrin.techie@gmail.com>
2025-06-07 13:27:20 +0530

    Changes on Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:moviez/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -17,10 +21,9 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         body: Center(
-          child: Text("Under Construction",textScaler: TextScaler.linear(6),),
+          child: Text("Under Construction", textScaler: TextScaler.linear(6)),
         ),
-      )
+      ),
     );
   }
 }
-
