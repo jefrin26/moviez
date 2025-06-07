2ce1fdc16eba2520af17706f587dd9ee3d42ea50
jefrin26 <jefrin.techie@gmail.com>
2025-06-07 12:53:34 +0530

    Added all Firebase based Services

--- lib/model/movie.dart
+++ lib/model/movie.dart
@@ -1,13 +1,13 @@
 class Movie {
   final String imdbID;
   final String title;
-  final String year;
-  final String poster;
 
-  Movie({
-    required this.imdbID,
-    required this.title,
-    required this.year,
-    required this.poster,
-  });
+  Movie({required this.imdbID, required this.title});
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      imdbID: json['imdbID'],
+      title: json['title']
+    );
+  }
 }
